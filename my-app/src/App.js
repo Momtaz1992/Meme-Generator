aa47736cc74e96e20ab2552bd9266eb18a683a32
Momtaz1992 <momtaz.mostafa.asmar@gmail.com>
2023-03-13 11:17:14 +0100

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import Meme from "./Meme";
+
+function App() {
+  return (
+    <div className="App">
+    <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Meme.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import React,{useState,useEffect} from 'react';
+function Meme() {
+       const [topText, setTopText] = useState('');
+       const [randomImg,setRandomImg] = useState('');
+       
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Meme
